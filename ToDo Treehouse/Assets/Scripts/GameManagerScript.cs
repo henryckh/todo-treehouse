b668599d305987ec2cbdd8fab3efad0aa7917064
thisisabhinay <a8h1nay@gmail.com>
2019-11-11 06:35:08 +0530

    Setups a unity 2D game project

---
+++ ToDo Treehouse/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public ToDoManager todoManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+    	todoManager.showAllTasks();
+        
+    }
+}
---
+++ ToDo Treehouse/Assets/Scripts/ToDoManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDoManager : MonoBehaviour
+{
+
+    private Task task = new Task("Work on game project", 80);
+
+    public void showAllTasks(){
+    	task.print();
+    }
+}
