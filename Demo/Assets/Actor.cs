b1afcf88e6c65939dd08cf81443d89163fe99784
thisisabhinay <a8h1nay@gmail.com>
2019-11-12 14:22:13 +0530

    Cleanup the project

---
+++ Demo/Assets/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+    int val;
+
+    public Actor() {
+        this.val = 5;
+    }
+
+    public void printVal() {
+        Debug.Log(val);
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
