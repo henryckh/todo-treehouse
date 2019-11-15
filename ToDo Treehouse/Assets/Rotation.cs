208bf4228f7b3a35cbfd4c6ee81d3f56272a0b1a
thisisabhinay <a8h1nay@gmail.com>
2019-11-16 02:07:34 +0530

    Rotates 'cube' on mouse over
    - Creates a rotation script and attach to container of cube
    - Adjusts the positioning and rotation of game objects in the level like
    camera, light etc.

---
+++ ToDo Treehouse/Assets/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public float rotateSpeed = 500.0f;
+    public GameObject cube;
+
+    private void Start() {
+        Debug.Log(cube);
+    }
+
+    private void Rotate() {
+        float rotateX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
+        float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+
+        cube.transform.Rotate(Vector3.up, -rotateX);
+        cube.transform.Rotate(Vector3.right, rotateY);
+    }
+
+    private void OnMouseOver() {
+        Rotate();
+    }
+
+    private void OnMouseDrag() {
+        Rotate();
+    }
+}
