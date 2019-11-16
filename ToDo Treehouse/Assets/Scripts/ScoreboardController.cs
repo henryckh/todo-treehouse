914dde3702995f5c74d6b2c08d11a24333d293dc
thisisabhinay <a8h1nay@gmail.com>
2019-11-16 20:18:38 +0530

    creates 'Scoreboard Controller'
    - Displays updated score text on UI

---
+++ ToDo Treehouse/Assets/Scripts/ScoreboardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreboardController : MonoBehaviour {
+    void UpdateXP(int num, int total) {
+        GameObject.Find("TextXp").GetComponent<Text>().text = "XP: " + num + "/" + total;
+    }
+
+    void UpdateLevel(int level) {
+        GameObject.Find("TextLevel").GetComponent<Text>().text = "Level: " + level;
+    }
+
+    void UpdateEnergy(int energy) {
+        GameObject.Find("TextEnergy").GetComponent<Text>().text = "Energy: " + energy;
+    }
+
+    public void UpdateScoreboard(Dictionary<string, int> stats) {
+        UpdateXP(stats["xp"], stats["next_xp"]);
+        UpdateLevel(stats["level"]);
+        UpdateEnergy(stats["energy"]);
+    }
+}
